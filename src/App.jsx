16909d4f453e52261acc25f0a5fd504d1dbfb8c7
rahatali555 <ali.rahat555@gmail.com>
2026-02-28 22:05:13 +0500

    start changing padding for experiment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App(){
   return (
     <div style={{
-      padding:20, fontFamily: "sans-serif"
+      padding:10, fontFamily: "sans-serif"
     }}>
       <h1>Workflow Builder</h1>
     </div>
