490419244c91a33f3fc4cc28bcc3c2f791b98d03
rahatali555 <ali.rahat555@gmail.com>
2026-02-28 22:06:03 +0500

    expermenting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App(){
   return (
     <div style={{
-      padding:10, fontFamily: "sans-serif"
+      padding:60, fontFamily: "sans-serif"
     }}>
       <h1>Workflow Builder</h1>
     </div>
