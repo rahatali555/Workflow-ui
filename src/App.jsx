2a6e937c938177edad03b0064b9a35fb41cea167
rahatali555 <ali.rahat555@gmail.com>
2026-03-08 16:12:54 +0500

    practice what i have learned yesterday

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import react from "react";
-import {
-  createRoot} from "react-dom/client";
-  const root= createRoot(document.querySelector("#root")).render(
-    <ul> 
-      <li>react</li>
-        <li>react</li>
-          <li>react</li>
-    </ul>
-  )
+import{ createRoot} from react-dom/client;
+const root=createRoot(document.querySelector('#root')).render(
+  <ol><li>react</li>
+    <li>react</li>
+    <li>react</li>
+  </ol>
+)
